0da8ff46b58b8886f6abf1b3f23062b8598bbad8
Rebelzob <karimdavid07@gmail.com>
2023-11-22 16:38:19 +0000

    Add devise authentication and associations to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable, :confirmable
+
+  has_many :expenses, foreign_key: :author_id, dependent: :destroy
+  has_many :categories, dependent: :destroy
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
 end
