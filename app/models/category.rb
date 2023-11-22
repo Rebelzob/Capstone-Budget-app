0251dae7261c1def2a7252db77eb70c6cf4972fe
Rebelzob <karimdavid07@gmail.com>
2023-11-22 16:37:18 +0000

    Added validation and relationships to Category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,7 @@
 class Category < ApplicationRecord
+  belongs_to :user
+  has_and_belongs_to_many :expenses
+
+  validate :name, presence: true, uniqueness: true
+  validate :icon, presence: true
 end
