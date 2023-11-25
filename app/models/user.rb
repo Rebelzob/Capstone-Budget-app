c79473dc4ae842a0ca04fe1c17219e541d4b70b9
Rebelzob <karimdavid07@gmail.com>
2023-11-25 01:40:37 +0000

    Refactor devise options in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
 
   has_many :expenses, foreign_key: :author_id, dependent: :destroy
   has_many :categories, dependent: :destroy
