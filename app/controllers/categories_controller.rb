de2df933874ca2f011dea2359eb02dd525ab3532
Rebelzob <karimdavid07@gmail.com>
2023-11-25 01:36:38 +0000

    Refactor categories controller to improve performance and readability

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,15 +1,14 @@
 class CategoriesController < ApplicationController
   before_action :set_category, only: %i[show]
-  before_action :set_user, only: %i[index show create]
 
   def index
-    @categories = @user.categories.includes(:expenses)
-    @expenses = @categories.flat_map { |category| category.expenses_for_user(current_user) }
+    @categories = current_user.categories.includes(:expenses)
+    @expenses = @categories.flat_map { |category| category.expenses.to_a }.select { |expense| expense.author_id == current_user.id }
   end
 
   def show
-    @expenses = @category.expenses_for_user(current_user).includes(:category)
-    @total = @expenses.sum(:amount)
+    @expenses = @category.expenses.where(author_id: current_user.id).order(created_at: :desc)
+    @total = @expenses.sum(&:amount)
   end
 
   def new
@@ -17,7 +16,7 @@ class CategoriesController < ApplicationController
   end
 
   def create
-    @category = @user.categories.build(category_params)
+    @category = current_user.categories.build(category_params)
 
     if @category.save
       redirect_to categories_path
@@ -30,10 +29,6 @@ class CategoriesController < ApplicationController
 
   def set_category
     @category = Category.find_by(id: params[:id])
-  end
-
-  def set_user
-    @user = current_user
   end
 
   def category_params
